0b6837aa68f25b79c88dc58b735b8085569f61e1
DanielNduka <nduka.daniel@omnibiz.com>
2025-07-04 01:34:02 +0100

    Update SelectBeneficiary screen to dynamically set header title based on payment action using useMemo for improved performance and clarity.

--- src/navigation/main-stack/screens/select-beneficiary/index.tsx
+++ src/navigation/main-stack/screens/select-beneficiary/index.tsx
@@ -1,5 +1,5 @@
 import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import React, { useMemo } from "react";
 import { TransactionScreenParams } from "@/types/transactions.types";
 import ScreenWrapper from "@/components/ui/screen-wrapper";
 import ScreenHeaderWithBack from "@/components/sections/screen-header-with-back";
@@ -7,9 +7,14 @@ import ScreenHeaderWithBack from "@/components/sections/screen-header-with-back"
 const SelectBeneficiary: React.FC<TransactionScreenParams> = ({ route }) => {
   const { paymentAction, amount } = route.params;
 
+  const screenTitle = useMemo(() => {
+    if (paymentAction === "request") return "Request from Beneficiary";
+    if (paymentAction === "send") return "Send";
+  }, [paymentAction]);
+
   return (
     <ScreenWrapper>
-      <ScreenHeaderWithBack title="" />
+      <ScreenHeaderWithBack title={screenTitle} />
     </ScreenWrapper>
   );
 };
